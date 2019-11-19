69e3df560047b43908d15660055c117c03335063
Elliot Gallagher <elliot@elliots-mbp.lan>
2019-11-19 16:39:39 -0500

    Added route for filtered quote list

--- index.js
+++ index.js
@@ -2,17 +2,23 @@ const express = require('express');
 
 const app = express();
 
-const quotes = require('./quotes/quotes.json');
+const QUOTES = require('./quotes/quotes.json');
 
 app.listen(5000, () => {
 	console.log('Server running on port 5000');
 });
 
 app.get('/random', (req, res) => {
-	var randomIndex = Math.floor(Math.random() * quotes.length);
-	res.send(quotes[randomIndex]);
+	let randomIndex = Math.floor(Math.random() * QUOTES.length);
+	res.send(QUOTES[randomIndex]);
 });
 
-app.get('/filter', (req, res) => {
-	res.send('Quote filter goes here');
+app.get('/filter/:keyword', (req, res) => {
+	let keyword = req.params.keyword.toLowerCase();
+
+	let filteredList = QUOTES.filter(item => {
+		item.quoteText.toLowerCase().search(keyword) === -1;
+	});
+
+	res.send(filteredList);
 });
