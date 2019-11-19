c3dd9d42b6ac1c383401fe0eae7c7be3b38b29e3
Elliot Gallagher <elliot@elliots-mbp.lan>
2019-11-19 12:04:34 -0500

    now serves random quotes

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000, () => {
-	console.log('Server running on port 3000');
+const quotes = require('./quotes/quotes.json');
+
+app.listen(5000, () => {
+	console.log('Server running on port 5000');
 });
 
-app.get('/', (req, res) => {
-	res.send('Server is running on port 3000');
+app.get('/random', (req, res) => {
+	var randomIndex = Math.floor(Math.random() * quotes.length);
+	res.send(quotes[randomIndex]);
+});
+
+app.get('/filter', (req, res) => {
+	res.send('Quote filter goes here');
 });
