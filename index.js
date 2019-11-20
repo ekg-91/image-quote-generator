bddf645b379e8402c4b7f04bd5ec5ba6068fd830
Elliot Gallagher <elliot@elliots-mbp.lan>
2019-11-20 11:12:15 -0500

    created requireToken middleware

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const QUOTES = require('./quotes/quotes.json');
 
+const requireToken = require('./middlewares/requireToken');
+
 const filterListByKeyword = (keyword) => QUOTES.filter(item =>
     item.quoteText.toLowerCase().search(keyword) !== -1 || 
     item.quoteAuthor.toLowerCase().search(keyword) !== -1);
@@ -14,12 +16,12 @@ app.listen(5000, () => {
   console.log('Server running on port 5000');
 });
 
-app.get('/random', (req, res) => {
+app.get('/random/:token', requireToken, (req, res) => {
   let randomIndex = Math.floor(Math.random() * QUOTES.length);
   res.send(QUOTES[randomIndex]);
 });
 
-app.get('/filter', (req, res) => {
+app.get('/filter/:token', requireToken, (req, res) => {
   const keyword = req.query.keyword.toLowerCase();
   const filteredList = filterListByKeyword(keyword);
 
---
+++ middlewares/requireToken.js
@@ -0,0 +1,8 @@
+module.exports = (req, res, next) => {
+  const TOKEN = 'QhYpswedg8';
+  if (req.params.token !== TOKEN) {
+    return res.status(401).send({ error: 'Incorrect or missing API token' });
+  }
+
+  next();
+}
