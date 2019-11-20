074a79db6f38bed5c952034d6935f6ce7acf4f51
Elliot Gallagher <elliot@elliots-mbp.lan>
2019-11-20 13:13:51 -0500

    added pagination functionality

--- index.js
+++ index.js
@@ -12,6 +12,25 @@ const shuffle = (array) => array.sort(() => Math.random() - 0.5);
 
 const random = (array) => shuffle(array)[0];
 
+const paginate = (array, page, per_page) => {
+  page = page || 1;
+  per_page = per_page || 10;
+  let offset = (page - 1) * per_page;
+
+  let paginated = array.slice(offset).slice(0, per_page);
+  let total_pages = Math.ceil(array.length / per_page);
+  
+  return {
+    page: page,
+    per_page: per_page,
+    pre_page: page - 1 ? page - 1 : null,
+    next_page: (total_pages > page) ? page + 1 : null,
+    total: array.length,
+    total_pages: total_pages,
+    data: paginated
+  };
+}
+
 app.listen(5000, () => {
   console.log('Server running on port 5000');
 });
@@ -23,8 +42,10 @@ app.get('/random/:token', requireToken, (req, res) => {
 
 app.get('/filter/:token', requireToken, (req, res) => {
   const keyword = req.query.keyword.toLowerCase();
+  const pageNum = req.query.pageNum;
   const filteredList = filterListByKeyword(keyword);
+  const paginatedList = paginate(filteredList, pageNum);
 
   if (req.query.random) res.send(random(filteredList));
-  else res.send(filteredList);
+  else res.send(paginatedList);
 });
--- middlewares/requireToken.js
+++ middlewares/requireToken.js
@@ -1,7 +1,8 @@
 module.exports = (req, res, next) => {
   const TOKEN = 'QhYpswedg8';
+
   if (req.params.token !== TOKEN) {
-    return res.status(401).send({ error: 'Incorrect or missing API token' });
+    return res.status(400).send({ error: 'Incorrect or missing API token' });
   }
 
   next();
