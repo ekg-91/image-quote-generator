ec8993da9b88b8ee34f4c75935fd54e2bf88f6e2
Elliot Gallagher <elliot@elliots-mbp.lan>
2019-11-19 20:09:41 -0500

    added logic to filter quotes with url keyword param

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const express = require('express');
-
 const app = express();
-
 const QUOTES = require('./quotes/quotes.json');
 
+const filterListByKeyword = (keyword) => QUOTES.filter(item =>
+    item.quoteText.toLowerCase().search(keyword) !== -1 || 
+    item.quoteAuthor.toLowerCase().search(keyword) !== -1);
+
+const shuffle = (array) => array.sort(() => Math.random() - 0.5);
+
+const random = (array) => shuffle(array)[0];
+
 app.listen(5000, () => {
-	console.log('Server running on port 5000');
+  console.log('Server running on port 5000');
 });
 
 app.get('/random', (req, res) => {
-	let randomIndex = Math.floor(Math.random() * QUOTES.length);
-	res.send(QUOTES[randomIndex]);
+  let randomIndex = Math.floor(Math.random() * QUOTES.length);
+  res.send(QUOTES[randomIndex]);
 });
 
-app.get('/filter/:keyword', (req, res) => {
-	let keyword = req.params.keyword.toLowerCase();
+app.get('/filter', (req, res) => {
+  const keyword = req.query.keyword.toLowerCase();
+  const filteredList = filterListByKeyword(keyword);
 
-	let filteredList = QUOTES.filter(item => {
-		item.quoteText.toLowerCase().search(keyword) === -1;
-	});
-
-	res.send(filteredList);
+  if (req.query.random) res.send(random(filteredList));
+  else res.send(filteredList);
 });
